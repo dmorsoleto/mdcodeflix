405bb7c2ca75889c0c654a70e8798ab772101093
Daniel Morsoleto <danielmorsoleto@pop-os.localdomain>
2020-07-30 22:47:19 -0300

    - url heroku

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,7 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const url = 'http://localhost:8080/categorias';
+    const url = 'https://mdcodeflix.herokuapp.com/categorias';
 
     fetch(url)
       .then(async (response) => {
